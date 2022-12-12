75941a04cd988eec4150dbc2ec51e21c0adc77d7
Anna Volfman <aniya_07@ukr.net>
2022-12-12 22:01:25 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+    return itemEl;
+})
+
+listEl.append(...elements);
+console.log(elements);
