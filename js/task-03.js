085c8ae9118f3d445792d64dcfa13b0634464fc2
Anna Volfman <aniya_07@ukr.net>
2022-12-13 18:13:20 +0200

    add task 3

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,21 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
+  
 ];
+
+
+const listEl = document.querySelector('.gallery');
+
+const makeImagesMarkup = ({url, alt}) => {
+  return `
+  <li class="gallery-item">
+  <img src="${url}" alt="${alt}" class="gallery-image">
+  </li>`;
+}
+
+const makeImages = images
+  .map(makeImagesMarkup)
+  .join(' '); 
+
+listEl.insertAdjacentHTML('afterbegin', makeImages);
