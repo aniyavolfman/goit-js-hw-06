b8a75cead92e8bfa2cf1d675762e7d7a3430cf65
Anna Volfman <aniya_07@ukr.net>
2022-12-14 21:48:29 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
+const formEl = document.querySelector('.login-form');
+const inputEl = document.querySelectorAll('input');
 
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    
+    const addAlert = () => {
+        
+        
+        if (inputEl[0].value === '' || inputEl[1].value === '') {
+            alert('Всі поля мають бути заповнені!!!');
+        } else {
+            const email = formElements.email.value;
+            const password = formElements.password.value;
+
+        const formData = {
+            email,
+            password,  
+        }
+            
+        console.log(formData);
+        }
+        
+    } 
+
+    addAlert();
+
+    event.currentTarget.reset();
+
+    
+}
