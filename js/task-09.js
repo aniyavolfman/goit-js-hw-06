e283492590806a46b0af26f144f2bc3d94bb7853
Anna Volfman <aniya_07@ukr.net>
2022-12-14 22:13:21 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+
+const addButtonChangeColor = buttonEl.addEventListener('click', (event) => {
+  
+  bodyEl.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+  spanEl.textContent = `${getRandomHexColor()}`;
+  
+})
