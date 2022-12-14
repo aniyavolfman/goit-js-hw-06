29a86bf0103509e5ec982197475b9a2d744523c9
Anna Volfman <aniya_07@ukr.net>
2022-12-14 18:51:39 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,13 @@ const spanEl = document.querySelector('#name-output');
 
 
 const addInputEventListener = inputEl.addEventListener('input', (event) => {
-    
     spanEl.textContent = event.currentTarget.value;
-    
+
+    const addDefaultName = () => {
+    if (event.currentTarget.value.length === 0) {
+    spanEl.textContent = 'Anonymous'};
+    }
+
+    addDefaultName();
 })
+
